7d194349f766d93acc415dfde587a7d12452d71b
Herman Guilliman <glmn.dev@protonmail.com>
2023-02-01 16:14:10 +0300

    add checking setttings in message handler

--- tgbot/handlers/ai_echo.py
+++ tgbot/handlers/ai_echo.py
@@ -6,13 +6,16 @@ from tgbot.models.aisettings import AISettings
 async def answer_ai(message: types.Message, openai: OpenAIService, repo: Repo):
     settings: AISettings = await repo.get_user_settings(message.from_id)
     await message.answer_chat_action(types.ChatActions.TYPING)
-    
-    ai_text_answer = await openai.get_answer(
-        max_tokens=settings.max_tokens,
-        model=settings.model,
-        temperature=settings.temperature,
-        prompt=message.text,
-        )
+    if settings is not None:
+        ai_text_answer = await openai.get_answer(
+            max_tokens=settings.max_tokens,
+            model=settings.model,
+            temperature=settings.temperature,
+            prompt=message.text,
+            )
+    else:
+        await message.answer('Настройки не найдены. Попробуйте выполнить /start')
+
     if ai_text_answer is not None:
         await message.answer(ai_text_answer, parse_mode='HTML')
     else:
