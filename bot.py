3306e99762d6f5af3ce17bc4b7b23bbcbe822b03
Herman Guilliman <glmn.dev@protonmail.com>
2023-10-23 21:18:22 +0300

    refactoring bot

--- bot.py
+++ bot.py
@@ -1,24 +1,29 @@
 import asyncio
-from loguru import logger
-from tgbot.config import load_config
 
-# database
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import sessionmaker
-
-# models
-from tgbot.models.base import Base
-from tgbot.models.user import Users
-from tgbot.models.settings import Settings
-from tgbot.models.personality import Personality
+# additional tools
+import openai
 
 # base aiogram with FSM
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.contrib.fsm_storage.redis import RedisStorage2
 
+# dialogs
+from aiogram_dialog import DialogRegistry
+from loguru import logger
+
+# database
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+
+from tgbot.config import load_config
+from tgbot.dialogs.main import main_dialog
+from tgbot.dialogs.neural import neural_chat
+from tgbot.dialogs.personality import person
+from tgbot.dialogs.settings import settings_dialog
+
 # filters
-from tgbot.filters.admin_filter import RoleFilter, AdminFilter
+from tgbot.filters.admin_filter import AdminFilter, RoleFilter
 
 # handlers
 from tgbot.handlers.admin_start import register_admin
@@ -28,15 +33,8 @@ from tgbot.handlers.user_start import register_user
 from tgbot.middlewares.db_and_repo import DbMiddleware
 from tgbot.middlewares.openai_api import OpenAIMiddleware
 
-# dialogs
-from aiogram_dialog import DialogRegistry
-from tgbot.dialogs.main import main_dialog
-from tgbot.dialogs.settings import settings_dialog
-from tgbot.dialogs.neural import neural_chat
-from tgbot.dialogs.personality import person
-
-# additional tools
-import openai
+# models
+from tgbot.models.base import Base
 
 
 async def create_sessionmaker(echo) -> AsyncSession:
@@ -49,10 +47,11 @@ async def create_sessionmaker(echo) -> AsyncSession:
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
     # session
-    session = sessionmaker(engine,
-                           expire_on_commit=False,
-                           class_=AsyncSession,
-                           )
+    session = sessionmaker(
+        engine,
+        expire_on_commit=False,
+        class_=AsyncSession,
+    )
     return session
 
 
@@ -75,12 +74,14 @@ async def main():
     logger.info("Starting ChimpAI")
     config = load_config(".env")
 
-    storage = RedisStorage2(
-        'redis', 6379, db=5, pool_size=10, prefix='chimpai_fsm'
-    ) if config.tg_bot.use_redis else MemoryStorage()
+    storage = (
+        RedisStorage2("redis", 6379, db=5, pool_size=10, prefix="chimpai_fsm")
+        if config.tg_bot.use_redis
+        else MemoryStorage()
+    )
     bot = Bot(token=config.tg_bot.token)
     dp = Dispatcher(bot, storage=storage)
-    bot['config'] = config
+    bot["config"] = config
 
     session = await create_sessionmaker(echo=False)
 
@@ -102,7 +103,7 @@ async def main():
         await bot.session.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
         asyncio.run(main())
     except (KeyboardInterrupt, SystemExit):
