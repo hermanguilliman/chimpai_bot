4169db7d732459628359f02f9480dd7635dad929
Herman Guilliman <hermanguilliman@proton.me>
2024-06-05 14:02:36 +0300

    remove apikey filter

--- tgbot/handlers/api_key.py
+++ tgbot/handlers/api_key.py
@@ -1,5 +1,3 @@
-import re
-
 from aiogram.enums import ParseMode
 from aiogram.types import Message
 from aiogram_dialog import DialogManager
@@ -14,16 +12,6 @@ async def api_key_handler(
     user_id = manager.bg().user.id
     repo: Repo = manager.middleware_data.get("repo")
     new_api_key = message.text
-    is_valid_key = bool(re.match("sk-[a-zA-Z0-9]{48}$", new_api_key))
-
-    if not is_valid_key:
-        await message.answer(
-            "⛔️ <b>Ошибка API ключа!</b>\nКлюч должен выглядеть как sk-...",
-            parse_mode=ParseMode.HTML,
-        )
-        await manager.done()
-        return
-
     manager.dialog_data["api_key"] = new_api_key
     await repo.update_api_key(user_id, new_api_key)
     await message.answer(
